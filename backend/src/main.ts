5d0e3520be84d4f05cc5447339c1bfa88a4f2cf1
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2026-08-15 19:36:44 +0500

    feat: initialize backend entry point with helmet, morgan, and cors middleware support

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Bootstrap');
+
+  // Security 
+  app.use(helmet());
+
+  // logging
+  app.use(morgan('combined'));
+
+  // cors
+  app.use(
+    cors({
+      origin: [process.env.FRONTEND_URL || 'http://localhost:3000/'],
+    })
+  )
+
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
