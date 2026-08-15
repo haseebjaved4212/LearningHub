c79d6126900aa6153f0214f335976e4bec7e5a87
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2026-08-15 19:39:54 +0500

    feat: initialize NestJS application with helmet, morgan, cors, and validation pipe configuration

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import cors from 'cors';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -19,9 +19,16 @@ async function bootstrap() {
   app.use(
     cors({
       origin: [process.env.FRONTEND_URL || 'http://localhost:3000/'],
+      credentials: true,
     })
   )
 
+  // validation
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true,
+  }));
 
   await app.listen(process.env.PORT ?? 3000);
 }
